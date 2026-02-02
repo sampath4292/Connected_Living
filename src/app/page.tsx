ef0a9109a106d1bf7167cd59a63bab4b08ad5401
sampath <sampathsriram4292@gmail.com>
2026-02-02 13:19:08 +0530

    project initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold underline">Connected Living</h1>
+      <p className="text-2xl">Welcome to Connected Living</p>
+      <p className="text-xl">Developed by : Sampath Sriram</p>
+    </div>
+  )
+}
